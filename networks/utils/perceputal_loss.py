8e15a0bff92a89a2fc094eb177aee4e26731e62d
Taiga Ishida <tonythetaiga@gmail.com>
2024-12-31 12:31:41 -0500

    allow specifying weights to layers used for perceptual loss

--- networks/utils/perceputal_loss.py
+++ networks/utils/perceputal_loss.py
@@ -8,7 +8,7 @@ class PerceptualLoss:
         self.feature_extractor.train(False)
         mx.eval(self.feature_extractor.parameters())
 
-    def __call__(self, x: mx.array, y: mx.array, layers: list[int] | None = None):
+    def __call__(self, x: mx.array, y: mx.array, layers: list[int] | None = None, weights: list[float] | None = None):
         # stack x and y and unstack after features are extracted?
         sample_features = self.feature_extractor.extract_features(x)  # pyright: ignore
         target_features = self.feature_extractor.extract_features(y)  # pyright: ignore
@@ -16,9 +16,12 @@ class PerceptualLoss:
         if layers is None:
             layers = list(range(len(sample_features)))
 
+        if weights is None:
+            weights = [1.0] * len(sample_features)
+
         loss = 0
-        for idx in layers:
+        for idx, weight in zip(layers, weights):
             x_f, y_f = sample_features[idx], target_features[idx]
-            loss += nn.losses.mse_loss(x_f, y_f, reduction="mean")
+            loss += weight * nn.losses.mse_loss(x_f, y_f, reduction="mean")
 
         return loss
