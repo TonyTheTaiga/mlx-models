e9466a75f6cfdf88804bf4d174e7d07c03726143
Taiga Ishida <tonythetaiga@gmail.com>
2025-06-16 15:48:44 -0400

    wip

--- training/ssd/main.py
+++ training/ssd/main.py
@@ -2,6 +2,8 @@ from __future__ import annotations
 from pathlib import Path
 
 import mlx.core as mx
+import mlx.nn as nn
+import mlx.optimizers as optim
 
 from networks.ssd.model import SSD300
 from networks.ssd.utils import generate_anchors, load_data, prepare_ssd_dataset
@@ -18,7 +20,17 @@ def dataloader(data, batch_size):
             data[2][idx[start : start + batch_size]],
         )
 
+
+def loss_fn(model, images: mx.array, loc_targets: mx.array, cls_targets: mx.array, alpha: float = 1.0):
+    loc_preds, cls_preds = model(images)
+    loc_loss = mx.mean(nn.losses.huber_loss(loc_preds, loc_targets))
+    cls_loss = mx.mean(nn.losses.cross_entropy(cls_preds, cls_targets))
+    return cls_loss + alpha * loc_loss
+
+
 def main():
+    learning_rate = 1e-3
+
     data = load_data(DATASET_ROOT, 300)
     anchors = generate_anchors([1, 2, 3, 1 / 2, 1 / 3], feature_map_sizes=[37, 18, 9, 5, 3, 1])
     dataset = prepare_ssd_dataset(data, anchors)
@@ -30,16 +42,25 @@ def main():
         strict=False,
     )
     mx.eval(model.parameters())
+    loss_and_grad_fn = nn.value_and_grad(model, loss_fn)
+    loc_optim = optim.AdamW(learning_rate=learning_rate)
+    cls_optim = optim.AdamW(learning_rate=learning_rate)
 
     image = mx.expand_dims(data[0]["resized_image"], 0)
     pred_loc, pred_cls = model(image)
 
-
     for epoch in range(5):
+        culm_loss = 0
+        num_samples = 0
         for images, loc_targets, cls_targets in dataloader(dataset, batch_size=4):
-            loc_predictions, cls_predictions = model(images)
-            print(loc_predictions.shape, cls_predictions.shape)
-            exit()
+            loss, grads = loss_and_grad_fn(model, images, loc_targets, cls_targets)
+            loc_optim.update(model, grads)
+            cls_optim.update(model, grads)
+            mx.eval(model.parameters(), loc_optim.state, cls_optim.state)
+            culm_loss += loss.item() * (images.shape[0])
+            num_samples += images.shape[0]
+
+        print(f"train loss @{epoch}", culm_loss / num_samples)
 
 
 if __name__ == "__main__":
