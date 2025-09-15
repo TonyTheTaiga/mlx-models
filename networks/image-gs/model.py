a57f413d8e1e4b584b6ada700a2db4459403e377
Taiga Ishida <tonythetaiga@gmail.com>
2025-09-15 19:01:59 +0900

    use softplus for log scale, add image-gs paper

--- networks/image-gs/model.py
+++ networks/image-gs/model.py
@@ -22,21 +22,21 @@ class Gaussian2D(nn.Module):
     def __init__(self, channels: int) -> None:
         super().__init__()
 
-        self.mean = mx.array([0, 0])
-        self.theta = mx.random.uniform(low=0, high=math.pi, shape=())
-        self.log_scales = mx.random.uniform(low=-5.0, high=-2.0, shape=(2,))
+        self.mean = mx.array([0.0, 0.0])
+        self.theta = mx.array(0.0)
+        self.log_scales = mx.random.uniform(low=-3.0, high=3.0, shape=(2,))
         self.c = mx.random.uniform(low=0.0, high=1.0, shape=(channels,))
 
     def __call__(self, x):
-        rm = rotation_matrix_2d(mx.clip(self.theta, 0.0, math.pi))
-        sm = scaling_matrix(mx.exp(self.log_scales))
+        rm = rotation_matrix_2d(math.pi * mx.sigmoid(self.theta))
+        eps = 1e-6
+        sm = scaling_matrix(nn.softplus(self.log_scales) + eps)
         cm = cov_from_r_and_s(rm, sm) + 1e-6 * mx.eye(2)
         d = x - self.mean
-        return mx.exp(-0.5 * (d @ mx.linalg.solve(cm, d, stream=mx.cpu))) * self.c  # pright: ignore
+        return mx.exp(-0.5 * (d @ mx.linalg.solve(cm, d, stream=mx.cpu))) * self.c  # pyright: ignore
 
 
 if __name__ == "__main__":
     g = Gaussian2D(3)
     X = mx.array([420, 69])
-
     print(g(X))
